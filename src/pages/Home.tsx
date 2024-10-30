3b125676f54f006e25c483edc7ea0bcbed5b39f5
yongseong2 <ancjs369@naver.com>
2024-10-30 22:32:38 +0900

    Init: 프론트엔트 초기 설정

---
+++ src/pages/Home.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { instance } from '../api/instance';
+
+export const Home = () => {
+  const [apiTest, setApiTest] = useState(false);
+  const [dbTest, setDbTest] = useState(false);
+
+  const handleApiOnclick = async () => {
+    try {
+      await instance.get('/test');
+      setApiTest(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleDbOnClick = async () => {
+    try {
+      await instance.get('/db');
+      setDbTest(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <main>
+      안녕하세요 해남 해녀입니다
+      <header className="App-header">
+        <div>Hello 해남해녀 배포 테스트 !</div>
+        <div>
+          <button onClick={handleApiOnclick}>API TEST</button>
+          <div>{apiTest ? 'CONNECT' : 'NOT YET'}</div>
+        </div>
+        <div>
+          <button onClick={handleDbOnClick}>DB TEST</button>
+          <div>{dbTest ? 'CONNECT' : 'NOT YET'}</div>
+        </div>
+      </header>
+    </main>
+  );
+};
