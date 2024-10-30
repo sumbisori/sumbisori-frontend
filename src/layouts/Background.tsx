51708b7d67500d47c15a3b73af79c8a0afde6e01
yongseong2 <ancjs369@naver.com>
2024-10-31 08:44:04 +0900

    Feat: 웹 페이지일때 하단에 로고 이미지 추가가

--- src/layouts/Background.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-interface Props {
-  children: ReactNode;
-}
-
-export const Background = ({ children }: Props) => {
-  return <main className="h-screen w-full bg-blue-500">{children}</main>;
-};
--- src/layouts/Mobile.tsx
+++ src/layouts/Mobile.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import { FullPageLogo } from '../components/FullPageLogo';
 
 interface Props {
   children: ReactNode;
@@ -6,8 +7,12 @@ interface Props {
 
 export default function Mobile({ children }: Props) {
   return (
-    <section className="m-auto flex h-screen w-full max-w-[393px] flex-col overflow-auto bg-slate-50">
-      {children}
-    </section>
+    <main className="h-screen w-full bg-blue-500">
+      {/* //왼쪽 하단에 로고이미지 추가 */}
+      <FullPageLogo />
+      <section className="m-auto flex h-screen w-full max-w-[393px] flex-col overflow-auto bg-slate-50">
+        {children}
+      </section>
+    </main>
   );
 }
