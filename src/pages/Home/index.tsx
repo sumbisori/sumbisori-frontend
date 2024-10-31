6ed3d37e33a1c9a5d62a127135143d3c168cd432
이용규 <gyu@iyong-gyuui-MacBookAir.local>
2024-10-31 14:05:04 +0900

    Feat: React-Unity-WebGL 추가

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,183 +5,27 @@ import { useModalContext } from '../../contexts/ModalContext';
 import { Modal } from '../../components/Modal';
 import { Button } from '../../components/Button';
 
+import { Unity, useUnityContext } from 'react-unity-webgl';
+
 export const Home = () => {
-  const [apiTest, setApiTest] = useState(false);
-  const [apiDB, setApiDB] = useState(false);
-  const [apiConnect, setApiConnect] = useState('');
-  const { openModal } = useModalContext();
-  // API 데이터 로딩 및 에러 상태 관리
-  const getTestMutation = useMutation({
-    mutationFn: getTest,
-    onSuccess: (data: boolean) => {
-      setApiTest(data);
-    },
-    onError: () => alert('Failed to connect'),
+  const { unityProvider, sendMessage } = useUnityContext({
+    loaderUrl: 'Build/build.loader.js',
+    dataUrl: 'Build/build.data.br',
+    frameworkUrl: 'Build/build.framework.js.br',
+    codeUrl: 'Build/build.wasm.br',
   });
 
-  // DB 테스트 버튼 처리
-  const getDbMutation = useMutation({
-    mutationFn: getDb,
-    onSuccess: (data: boolean) => {
-      setApiDB(data);
-    },
-    onError: () => alert('DB Test Failed!'),
-  });
-
-  // API 테스트 버튼 처리
-  const getConnectMutation = useMutation({
-    mutationFn: getConnect,
-    onSuccess: () => {
-      alert('API Test Success!');
-      setApiConnect('API Connect Success!');
-    },
-    onError: () => alert('API Test Failed!'),
-  });
-
-  const handleApiTest = () => {
-    getTestMutation.mutate();
-  };
-
-  const handleDbTest = () => {
-    getDbMutation.mutate();
-  };
-
-  const handleApiConnect = () => {
-    getConnectMutation.mutate();
-  };
-
-  const handleModal = () => {
-    openModal('test-modal');
-  };
+  function handleClickButton() {
+    sendMessage('Init Manager', 'Init', 'BlueFin3');
+  }
 
   return (
-    <main>
-      <div>안녕하세요 해남 해녀입니다</div>
-      <header>
-        <div className="bg-blue-600 text-h3 text-orange-400">
-          Hello 해남해녀 배포 테스트!
-        </div>
-        <div className="m-xs"></div>
-
-        <div>
-          <Button onClick={handleModal}>Modal TEST</Button>
-          <Modal id="test-modal" title="Modal Test">
-            <div>Modal Content</div>
-          </Modal>
-        </div>
-
-        <div>
-          <Button onClick={handleApiTest}>API TEST</Button>
-
-          <div>
-            {getTestMutation.isPending ? 'Testing...' : apiTest}
-            {getTestMutation.isError ? 'Failed to connect' : ''}
-          </div>
-        </div>
-
-        <div>
-          <Button onClick={handleDbTest}>DB TEST</Button>
-          <div>
-            {getDbMutation.isPending ? 'Testing...' : apiDB}
-            {getDbMutation.isError ? 'Failed to connect' : ''}
-          </div>
-        </div>
-
-        <div>
-          <Button onClick={handleApiConnect}>API CONNECT</Button>
-          <div>
-            {getConnectMutation.isPending ? 'Testing...' : apiConnect}
-            {getConnectMutation.isError ? 'Failed to connect' : ''}
-          </div>
-        </div>
-        <div className="flex">
-          <div>
-            <div className="size-10 bg-gray-000"></div>
-            <div className="size-10 bg-gray-050"></div>
-            <div className="size-10 bg-gray-100"></div>
-            <div className="size-10 bg-gray-200"></div>
-            <div className="size-10 bg-gray-300"></div>
-            <div className="size-10 bg-gray-400"></div>
-            <div className="size-10 bg-gray-500"></div>
-            <div className="size-10 bg-gray-600"></div>
-            <div className="size-10 bg-gray-700"></div>
-            <div className="size-10 bg-gray-800"></div>
-            <div className="size-10 bg-gray-900"></div>
-          </div>
-          <div>
-            <div className="size-10 bg-red-050"></div>
-            <div className="size-10 bg-red-100"></div>
-            <div className="size-10 bg-red-200"></div>
-            <div className="size-10 bg-red-300"></div>
-            <div className="size-10 bg-red-400"></div>
-            <div className="size-10 bg-red-500"></div>
-            <div className="size-10 bg-red-600"></div>
-            <div className="size-10 bg-red-700"></div>
-            <div className="size-10 bg-red-800"></div>
-            <div className="size-10 bg-red-900"></div>
-          </div>
-          <div>
-            <div className="size-10 bg-cyan-050"></div>
-            <div className="size-10 bg-cyan-100"></div>
-            <div className="size-10 bg-cyan-200"></div>
-            <div className="size-10 bg-cyan-300"></div>
-            <div className="size-10 bg-cyan-400"></div>
-            <div className="size-10 bg-cyan-500"></div>
-            <div className="size-10 bg-cyan-600"></div>
-            <div className="size-10 bg-cyan-700"></div>
-            <div className="size-10 bg-cyan-800"></div>
-            <div className="size-10 bg-cyan-900"></div>
-          </div>
-          <div>
-            <div className="size-10 bg-pink-050"></div>
-            <div className="size-10 bg-pink-100"></div>
-            <div className="size-10 bg-pink-200"></div>
-            <div className="size-10 bg-pink-300"></div>
-            <div className="size-10 bg-pink-400"></div>
-            <div className="size-10 bg-pink-500"></div>
-            <div className="size-10 bg-pink-600"></div>
-            <div className="size-10 bg-pink-700"></div>
-            <div className="size-10 bg-pink-800"></div>
-            <div className="size-10 bg-pink-900"></div>
-          </div>
-          <div>
-            <div className="size-10 bg-green-050"></div>
-            <div className="size-10 bg-green-100"></div>
-            <div className="size-10 bg-green-200"></div>
-            <div className="size-10 bg-green-300"></div>
-            <div className="size-10 bg-green-400"></div>
-            <div className="size-10 bg-green-500"></div>
-            <div className="size-10 bg-green-600"></div>
-            <div className="size-10 bg-green-700"></div>
-            <div className="size-10 bg-green-800"></div>
-            <div className="size-10 bg-green-900"></div>
-          </div>
-          <div>
-            <div className="size-10 bg-grape-050"></div>
-            <div className="size-10 bg-grape-100"></div>
-            <div className="size-10 bg-grape-200"></div>
-            <div className="size-10 bg-grape-300"></div>
-            <div className="size-10 bg-grape-400"></div>
-            <div className="size-10 bg-grape-500"></div>
-            <div className="size-10 bg-grape-600"></div>
-            <div className="size-10 bg-grape-700"></div>
-            <div className="size-10 bg-grape-800"></div>
-            <div className="size-10 bg-grape-900"></div>
-          </div>
-          <div>
-            <div className="size-10 bg-lime-050"></div>
-            <div className="size-10 bg-lime-100"></div>
-            <div className="size-10 bg-lime-200"></div>
-            <div className="size-10 bg-lime-300"></div>
-            <div className="size-10 bg-lime-400"></div>
-            <div className="size-10 bg-lime-500"></div>
-            <div className="size-10 bg-lime-600"></div>
-            <div className="size-10 bg-lime-700"></div>
-            <div className="size-10 bg-lime-800"></div>
-            <div className="size-10 bg-lime-900"></div>
-          </div>
-        </div>
-      </header>
-    </main>
+    <div>
+      <Unity
+        unityProvider={unityProvider}
+        style={{ width: 393, height: 852 }}
+      />
+      <button onClick={handleClickButton}></button>
+    </div>
   );
 };
