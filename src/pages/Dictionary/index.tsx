08343360a9e85d21dd76034d1684040bee5b395c
이용규 <gyu@iyong-gyuui-MacBookAir.local>
2024-10-31 21:08:32 +0900

    Feat: Fix Complete

--- src/pages/Dictionary/index.tsx
+++ src/pages/Dictionary/index.tsx
@@ -21,9 +21,9 @@ export const Dictionary = () => {
   return (
     <Background>
       <div className="flex size-full flex-col">
-        <div className="h-24 bg-gray-600"></div>
+        <div className="h-24 bg-gray-600"></div>z
         <div className="size-full grid-cols-3 gap-[2px] border-orange-200 bg-orange-100">
-          <SeafoodCard props={{ name: 'Oyster', has: false }} />
+          {/* <SeafoodCard props={{ name: 'Oyster', has: false }} /> */}
         </div>
       </div>
     </Background>
