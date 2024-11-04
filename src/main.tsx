267c4066c22da7b1043575a6548a4cef5c8dc1f8
yongseong2 <ancjs369@naver.com>
2024-11-04 23:50:59 +0900

    Fix: 배포경로 테스트 수정

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename={import.meta.env.DEV ? '/' : '/sumbisori-frontend/'}>
+  <BrowserRouter basename={import.meta.env.PROD ? '/sumbisori-frontend' : '/'}>
     <App />
   </BrowserRouter>,
 );
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/sumbisori-frontend/' : '/',
+  base: '/',
   plugins: [react()],
   server: {
     port: 3000,
