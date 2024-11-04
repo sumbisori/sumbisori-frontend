7a7cd8155e2e2a2d7e935ce0a4660278606d162b
yongseong2 <ancjs369@naver.com>
2024-11-04 22:59:05 +0900

    Release: 배포테스트

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/sumbisori-frontend/',
   plugins: [react()],
   server: {
     port: 3000,
