4185ddb6790276d7a4ed7ae1f659dd574dbccbb2
yongseong2 <ancjs369@naver.com>
2024-11-05 12:35:21 +0900

    Fix: API경로 secret으로 뺌

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { store } from '../store';
 
-const API_URL = 'https://k1ec344612739a.user-app.krampoline.com/api';
+const API_URL = import.meta.env.VITE_API_URL;
 
 const instance = axios.create({
   baseURL: API_URL,
