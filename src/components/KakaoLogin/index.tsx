4956a0396491d72191c0f26c8654bddf57235bb8
yongseong2 <ancjs369@naver.com>
2024-11-14 14:23:42 +0900

    Chore: 로그인 api 주소 변경

--- src/components/KakaoLogin/index.tsx
+++ src/components/KakaoLogin/index.tsx
@@ -17,7 +17,7 @@ interface Props extends React.AnchorHTMLAttributes<HTMLAnchorElement> {}
 export const KakaoLogin = ({ ...props }: Props) => {
   return (
     <a
-      href="https://k1ec344612739a.user-app.krampoline.com/api/oauth2/authorization/kakao"
+      href="https://api.sumbisori.site/api/oauth2/authorization/kakao"
       className="block h-[45px] w-[183px] bg-cover bg-center bg-no-repeat"
       {...props}
       style={{
