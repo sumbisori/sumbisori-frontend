011510f5d0018fab05680596d609a4f6fe5c6111
yongseong2 <ancjs369@naver.com>
2025-01-23 13:14:11 +0900

    Fix: 시간 util 함수 수정

--- src/util/convertToTodayTime.ts
+++ src/util/convertToTodayTime.ts
@@ -3,5 +3,5 @@ export function convertToTodayTime(datetimeStr: string) {
   const hours = date.getHours().toString().padStart(2, '0');
   const minutes = date.getMinutes().toString().padStart(2, '0');
 
-  return `${hours}:${minutes}`;
+  return datetimeStr ? `${hours}:${minutes}` : '--:--';
 }
