71ddfe02fa9768e2620b211f56415e74381636ec
yongseong2 <ancjs369@naver.com>
2025-02-18 09:50:05 +0900

    Feat: 안쓰는 페이지 삭제

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,8 +6,6 @@ import { HaenyeoPlaces } from '@/pages/HaenyeoPlaces';
 import { Dictionary } from '@/pages/Dictionary';
 import { MyPage } from '@/pages/MyPage';
 import { MyPageReservation } from '@/pages/MyPageReservation';
-import { DictionaryRegistration } from '@/pages/DictionaryRegistration';
-import { DictionaryConfirm } from '@/pages/DictionaryConfirm';
 import { MobileLayout } from '@/layouts/MobileLayout';
 import ScrollToTop from './ScrollToTop';
 import { HaenyeoPlacesDetail } from '@/pages/HaenyeoPlacesDetail';
@@ -27,14 +25,6 @@ export const Router = () => {
             <Route path="/haenyeo-places" element={<HaenyeoPlaces />} />
 
             <Route path="/dictionary" element={<Dictionary />} />
-            <Route
-              path="/dictionary/registration"
-              element={<DictionaryRegistration />}
-            />
-            <Route
-              path="/dictionary/confirm/:seafood/:koreanName"
-              element={<DictionaryConfirm />}
-            />
             <Route path="/my-page" element={<MyPage />} />
             <Route
               path="/my-page/reservation"
